721d47db802bb6eae5307c4e8c8a9c925befde66
Mykola Kokoiev <veokoknik@gmail.com>
2025-01-08 13:01:59 +0100

    add index.html tamplate & add pass data in context

--- manager/views.py
+++ manager/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from .models import Worker, Task
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<html>"
-                        "<h1>"
-                        "Welcome to homepage"
-                        "</h1>"
-                        "</html>")
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+    context = {
+        "num_workers": num_workers,
+        "num_tasks": num_tasks,
+    }
+
+    return render(request, 'manager/index.html', context=context)
