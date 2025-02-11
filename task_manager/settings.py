890ed1ef5435237a558bf634208972d61f4e52b1
Mykola Kokoiev <veokoknik@gmail.com>
2025-02-11 06:50:38 +0100

    add 'crispy_forms', 'crispy_bootstrap4' INSTALLED_APPS & CRISPY_TEMPLATE_PACK = 'bootstrap4' in 'settings.py'

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "manager",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -136,3 +138,5 @@ LOGIN_REDIRECT_URL = "/manager/"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
