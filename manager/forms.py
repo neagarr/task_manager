1563472e0cb2fc2a6ad0db97dca25ba21861e64f
Mykola Kokoiev <veokoknik@gmail.com>
2025-02-12 11:00:21 +0100

    rename 'WorkerCreationForm' to 'WorkerRegistrationForm'

--- manager/forms.py
+++ manager/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from manager.models import Worker
 
 
-class WorkerCreationForm(UserCreationForm):
+class WorkerRegistrationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Worker
         fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "position",)
