98258a90f8cc3afd44bec4f0304694a571becb53
Mykola Kokoiev <veokoknik@gmail.com>
2025-02-14 07:10:37 +0100

    add 'ContextMixin' in manager/mixins.py

--- manager/mixins.py
+++ manager/mixins.py
@@ -1,27 +1,14 @@
 from manager.forms import SearchForm
 
 
-# class ContextMixin:
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super(TaskTypeListView, self).get_context_data(**kwargs)
-#         title = self.request.GET.get("title", "")
-#         context["title"] = title
-#         context["search_form"] = SearchForm(
-#             initial={"title": title}
-#         )
-#         return context
-
-
-# class QuerysetMixin:
-#     def __init__(self, name, queryset):
-#         super().__init__(name, queryset)
-#
-#     @staticmethod
-#     def get_queryset():
-#         title = super().request.GET.get("title")
-#         if title:
-#             return queryset.filter(name__icontains=title)
-#         return queryset
+class ContextMixin:
+    def get_context_data_mixin(self, context):
+        title = self.request.GET.get("title", "")
+        context["title"] = title
+        context["search_form"] = SearchForm(
+            initial={"title": title}
+        )
+        return context
 
 
 class QuerysetMixin:
