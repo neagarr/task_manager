46607d59873fd7c91b7af46e8bcf7aea0ae7a0ff
Mykola Kokoiev <veokoknik@gmail.com>
2025-02-26 20:17:38 +0100

    add 'PrivateTaskTest' in 'test_task_views.py'

--- manager/tests/test_task_views.py
+++ manager/tests/test_task_views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
@@ -10,15 +11,15 @@ TASK_LIST_URL = reverse("manager:task_list")
 
 class PublicTaskTest(TestCase):
     def setUp(self):
-        test_type = TaskType.objects.create(
-            name="Test Type"
+        test_task_type = TaskType.objects.create(
+            name="Test Task Type"
         )
         self.task = Task.objects.create(
             name="Test Task",
             description="Test Description",
             deadline=datetime.now(),
             priority="Urgent",
-            type=test_type
+            type=test_task_type
         )
 
     def test_task_list_login_required(self):
@@ -30,3 +31,36 @@ class PublicTaskTest(TestCase):
         res = self.client.get(url)
         self.assertNotEqual(res.status_code, 200)
 
+
+class PrivateTaskTest(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="test_username",
+            password="test_password",
+        )
+        test_task_type = TaskType.objects.create(
+            name="Test Task Type"
+        )
+        self.task = Task.objects.create(
+            name="Test Task",
+            description="Test Description",
+            deadline=datetime.now(),
+            priority="Urgent",
+            type=test_task_type
+        )
+        self.client.force_login(self.user)
+
+    def test_retrieve_worker_list(self):
+        res = self.client.get(TASK_LIST_URL)
+        self.assertEqual(res.status_code, 200)
+        tasks = Task.objects.all()
+        self.assertEqual(
+            list(res.context["task_list"]),
+            list(tasks),
+        )
+        self.assertTemplateUsed(res, "manager/task_list.html")
+
+    def test_retrieve_worker_detail(self):
+        res = self.client.get(reverse("manager:task_detail", args=[self.task.id]))
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, "manager/task_detail.html")
