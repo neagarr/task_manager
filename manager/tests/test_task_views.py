a2b0385882163f89850ae74c5621d40fbbabd7ba
Mykola Kokoiev <veokoknik@gmail.com>
2025-02-26 12:23:39 +0100

    add 'test_task_detail_login_required' in 'test_task_views.py'

--- manager/tests/test_task_views.py
+++ manager/tests/test_task_views.py
@@ -1,10 +1,31 @@
+from datetime import datetime
+
 from django.test import TestCase
 from django.urls import reverse
+
+from manager.models import TaskType, Task
 
 TASK_LIST_URL = reverse("manager:task_list")
 
 
 class PublicTaskTest(TestCase):
+    def setUp(self):
+        test_type = TaskType.objects.create(
+            name="Test Type"
+        )
+        self.task = Task.objects.create(
+            name="Test Task",
+            description="Test Description",
+            deadline=datetime.now(),
+            priority="Urgent",
+            type=test_type
+        )
+
     def test_task_list_login_required(self):
         res = self.client.get(TASK_LIST_URL)
+        self.assertNotEqual(res.status_code, 200)
+
+    def test_task_detail_login_required(self):
+        url = reverse("manager:task_detail", args=[self.task.id])
+        res = self.client.get(url)
         self.assertNotEqual(res.status_code, 200)
