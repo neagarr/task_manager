a7fbf628fd2723a490bd734a9f14b5bd3529fdce
Mykola Kokoiev <veokoknik@gmail.com>
2025-02-27 08:14:18 +0100

    add 'test_create_position', 'test_update_position', 'test_delete_position'

--- manager/tests/test_position_views.py
+++ manager/tests/test_position_views.py
@@ -31,3 +31,21 @@ class PrivatePositionTest(TestCase):
             list(positions),
         )
         self.assertTemplateUsed(res, "manager/position_list.html")
+
+    def test_create_position(self):
+        form_data = {"name": "Test Create"}
+        self.client.post(reverse("manager:position_create"), form_data)
+        new_position = Position.objects.get(name="Test Create")
+
+        self.assertEqual(new_position.name, form_data["name"])
+
+    def test_update_position(self):
+        position_id = self.position.id
+        form_data = {"name": "Test Update"}
+        self.client.post(reverse("manager:position_update", args=[position_id]), form_data)
+        self.assertEqual(Position.objects.get(id=position_id).name, form_data["name"])
+
+    def test_delete_position(self):
+        position_id = self.position.id
+        self.client.post(reverse("manager:position_delete", args=[position_id]), {})
+        self.assertEqual(Position.objects.filter(id=position_id).count(), 0)
